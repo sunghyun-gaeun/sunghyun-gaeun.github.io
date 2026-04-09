e33288c3303b9b0f44265dde07aa7422de8e20a4
sunghyun-gaeun <zionjair@gmail.com>
2026-04-09 23:23:58 +0900

    Update Simple.tsx

--- src/layout/Layouts/Simple.tsx
+++ src/layout/Layouts/Simple.tsx
@@ -91,7 +91,7 @@ const SimpleLayout = ({ id, data }: LayoutProp) => {
       </motion.div>
       <motion.div>
       <Wrapper>
-        <NeonSign text={`화환은 로비 공간이 부족하니,\n마음만 보내주세요`} />
+        <Heading1 text={`화환은 로비 공간이 부족하니,\n마음만 보내주세요`} />
       </Wrapper>
       </motion.div>
       <motion.div
